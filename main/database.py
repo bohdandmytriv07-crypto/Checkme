e4810fd13dc3f6ece8d0f60b130fc2d761e4522a
Bohdan <bohdandmytriv07@gmail.com>
2026-04-08 15:14:32 +0300

    Update database.py

--- main/database.py
+++ main/database.py
@@ -1,11 +1,11 @@
-﻿# -*- coding: utf-8 -*-
+﻿
 import sqlite3
 from datetime import datetime
 
 DB_NAME = "cache.db"
 
 def init_db():
-    """Створює таблицю з лічильником перевірок"""
+
     with sqlite3.connect(DB_NAME) as conn:
         cursor = conn.cursor()
         cursor.execute('''
@@ -21,7 +21,7 @@ def init_db():
         conn.commit()
 
 def get_cached_info(url):
-    """Повертає всю інформацію про посилання, включаючи кількість перевірок"""
+
     with sqlite3.connect(DB_NAME) as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT status, title, message, check_count FROM url_cache WHERE url=?", (url,))
@@ -36,18 +36,15 @@ def get_cached_info(url):
         return None
 
 def save_or_update_cache(url, result):
-    """
-    Якщо посилання нове — створює запис (count=1).
-    Якщо старе — оновлює дані та збільшує лічильник на +1.
-    """
+
     with sqlite3.connect(DB_NAME) as conn:
         cursor = conn.cursor()
-        # Спочатку перевіримо, чи є вже такий запис
+  
         cursor.execute("SELECT check_count FROM url_cache WHERE url=?", (url,))
         row = cursor.fetchone()
         
         if row is None:
-            # Нове посилання
+    
             cursor.execute('''
                 INSERT INTO url_cache (url, status, title, message, last_updated, check_count)
                 VALUES (?, ?, ?, ?, ?, ?)
